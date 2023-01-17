8b381941873f5354932f169f4340c630a9464403
oNqNu <onanukintaro@gmail.com>
2023-01-17 16:43:17 +0900

    feat: formItemのアセットを作成

---
+++ pages/assets.ts
@@ -0,0 +1,8 @@
+export const formItems = [
+  { label: 'data', name: 'QRコードに格納するデータ' },
+  { label: 'version', name: 'QRコードのバージョン' },
+  { label: 'ecc_level', name: 'ecc_level' },
+  { label: 'mask_pattern', name: 'マスクパターン' },
+  { label: 'threshold', name: 'threshold' },
+  { label: 'variance', name: 'variance' },
+];

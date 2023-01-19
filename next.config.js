0dd306589df61a42866456d0c3a65139f8fe4378
oNqNu <onanukintaro@gmail.com>
2023-01-19 17:44:31 +0900

    github pages 設定

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/project_name' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

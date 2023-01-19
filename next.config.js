42e6936f6316e8e003c0f3ed9df8cb72f67c8181
oNqNu <onanukintaro@gmail.com>
2023-01-19 17:53:48 +0900

    assetPrefix: '/qrcode-app-frontend' の設定

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/project_name' : '',
+  assetPrefix: '/qrcode-app-frontend',
 };
 
 module.exports = nextConfig;

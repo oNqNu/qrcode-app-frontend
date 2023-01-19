cfa0510bf6d8fb3ac80bc2dcaa8958adf97e3e9a
oNqNu <onanukintaro@gmail.com>
2023-01-19 20:58:20 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: process.env.GITHUB_PAGES ? '/repository-name' : '',
+  assetPrefix: process.env.GITHUB_PAGES ? '/qrcode-app-frontend' : '',
 };
 
 module.exports = nextConfig;

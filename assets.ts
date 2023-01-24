a2cabebf9f752f4007be48f040904e0db6a07ff2
oNqNu <onanukintaro@gmail.com>
2023-01-24 13:49:03 +0900

    表示するフォームのアイテムを変更

--- assets.ts
+++ assets.ts
@@ -1,7 +1,6 @@
 export const formItems = [
   { label: 'data', name: 'QRコードに格納するデータ' },
   { label: 'version', name: 'QRコードのバージョン' },
-  { label: 'ecc_level', name: '誤り訂正レベル' },
   { label: 'mask_pattern', name: 'マスクパターン' },
   { label: 'threshold', name: 'threshold' },
   { label: 'variance', name: 'variance' },
